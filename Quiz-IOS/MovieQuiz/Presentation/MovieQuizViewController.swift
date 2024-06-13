89d27fff09a894a3512eb2a5322450c639b24b26
serg <gerasichev.serg@gmail.com>
2024-06-13 15:32:20 +0300

    .

--- Quiz-IOS/MovieQuiz/Presentation/MovieQuizViewController.swift
+++ Quiz-IOS/MovieQuiz/Presentation/MovieQuizViewController.swift
@@ -41,7 +41,7 @@ final class MovieQuizViewController: UIViewController, QuestionFactoryDelegate {
             self.currentQuestionIndex = 0
             self.correctAnswers = 0
             
-            self.questionFactory?.requestNextQuestion()
+            self.questionFactory?.loadData()
         }
         
         alertPresenter.showAlert(model: model)
